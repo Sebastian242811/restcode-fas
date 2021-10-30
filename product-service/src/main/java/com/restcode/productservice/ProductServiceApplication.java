1d19f673130e5f320324e3584dc0b46fcfea186b
Sebastian242811 <hernandesebastian0@gmail.com>
2021-10-30 12:47:50 -0500

    Adding eureka clients to microservices

--- product-service/src/main/java/com/restcode/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/restcode/productservice/ProductServiceApplication.java
@@ -2,17 +2,12 @@ package com.restcode.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EntityScan("com.restcode.productservice.domain.model")
-@EnableJpaRepositories("com.restcode.productservice.domain.repository")
-//@EnableEurekaClient
+@EnableEurekaClient
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
--- restaurant-service/src/main/java/com/restcode/restaurantservice/RestaurantServiceApplication.java
+++ restaurant-service/src/main/java/com/restcode/restaurantservice/RestaurantServiceApplication.java
@@ -2,10 +2,12 @@ package com.restcode.restaurantservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableEurekaClient
 public class RestaurantServiceApplication {
 
     public static void main(String[] args) {
