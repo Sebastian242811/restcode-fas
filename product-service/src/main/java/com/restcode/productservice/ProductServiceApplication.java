28b77af7db233240f1c1f98aff1eeee9a7517d44
Sebastian242811 <hernandesebastian0@gmail.com>
2021-11-25 23:19:34 -0500

    changing config data 1

--- product-service/src/main/java/com/restcode/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/restcode/productservice/ProductServiceApplication.java
@@ -3,6 +3,8 @@ package com.restcode.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -10,6 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @EnableEurekaClient
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
